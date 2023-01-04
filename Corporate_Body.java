cc8f7a8069436e6ebf3850e23b4f74920257ffd0
Henry Ardern <39902585+h-ardern@users.noreply.github.com>
2023-01-04 15:35:23 +0000

    Update Corporate_Body Added constructor and the ability to add employees later after initialization.

--- Corporate_Body.java
+++ Corporate_Body.java
@@ -6,5 +6,17 @@ public class Corporate_Body {
     String Name;
     HashSet<String> Employees;
 
+        public Corporate_Body(String name,ArrayList<String> emps){
+            this.Name = name;
+            for(int i = 0 ; i < emps.size(); i++){
+                this.Employees.add(emps.get(i));
+            }
+
+        }
+
+        public void add_Employee(String emp){
+            Employees.add(emp);
+        }
+
 
 }
