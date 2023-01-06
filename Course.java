551109659cf4445f8743a0e1f21c282bab53f701
Henry Ardern <39902585+h-ardern@users.noreply.github.com>
2023-01-06 15:25:04 +0000

    Updated Session and started Course

--- Course.java
+++ Course.java
@@ -1,2 +1,21 @@
+import java.util.*;
+
 public class Course {
+    int ID;
+    String Name;
+    HashMap<Integer,Session> Contents;
+    int Price;
+
+    public Course(int id ,String name, ArrayList<Session> contents,int price){
+        this.ID = id;
+        this.Name = name;
+        for(int i = 0; i < contents.size();i ++){
+            Session Sess = contents.get(i);
+            this.Contents.put(Sess.ID,Sess);
+
+        }
+        this.Price = price;
+
+    }
+    public.v
 }
--- Session.java
+++ Session.java
@@ -1,11 +1,13 @@
 
 public class Session {
+    int ID;
     String Name;
     Object Contents;
     Summary_Data Summary;
     Boolean Completed;
 
-    public Session(String name , Object contents, Summary_Data summary){
+    public Session(int id,String name , Object contents, Summary_Data summary){
+        this.ID =id;
         this.Name = name;
         this.Contents = contents;
         this.Summary = summary;
