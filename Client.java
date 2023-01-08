263cac121ae31ab92704864ae7660b41d58cf2ac
KayJay02 <122222665+KayJay02@users.noreply.github.com>
2023-01-08 17:57:29 +0000

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,3 +1,30 @@
 public class Client {
-
+int ID;
+    String firstName;
+    String lastName;
+    int Age;
+    HashSet<Integer> Consultant;
+    HashSet<Course> Course;
+  
+    enum Level{
+        Corporate,
+        Individual
+    }
+    
+    public Client(int id, String firstname, String lastname, int age, ArrayList<Integer> con1, ArrayList<Course> crs)
+    {
+        this.ID = id;
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.Age = age;
+        for(int i = 0; i < con1.size(); i++)
+        {
+            this.Consultant.add(con1.get(i));
+        }
+        for(int i = 0; i < crs.size(); i++)
+        {
+            this.Course.add(crs.get(i));
+        }
+        
+    }
 }
