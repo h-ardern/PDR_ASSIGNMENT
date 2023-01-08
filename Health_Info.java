593a31fa7e84abed1620a1c4c5a30224d2da7ad1
jmccarthyy <121821019+jmccarthyy@users.noreply.github.com>
2023-01-08 18:40:03 +0000

    Update Health_Info.java

--- Health_Info.java
+++ Health_Info.java
@@ -1,8 +1,11 @@
 public class Health_Info {
   
+  
+  public class Health_Condition{
   public static void main(String[] args) {
     HashSet<String> condition = new HashSet<String>();
-   
+    //rest of hashset
     System.out.println(condition);
+  }
   }
 }
