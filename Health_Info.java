6c338b27d58ce7d3f55589374043d4ce0c67a759
jmccarthyy <121821019+jmccarthyy@users.noreply.github.com>
2023-01-08 22:04:06 +0000

    Update Health_Info.java

--- Health_Info.java
+++ Health_Info.java
@@ -1,9 +1,27 @@
 import java.util.HashSet;
 
-public class Health_Info {
-  public static void main(String[] args) {
-    HashSet condition = new HashSet();
-    //rest of hashset
-  }
+public class Health_info
+{
+    public Health_info() 
+    {
+
+        this.Conditions = new HashSet<String>() ; 
+        this.PastSessions = new ArrayList<String>() ;
+    }
+    public void addClientConditions(String ClientID ) 
+        {
+
+            if( this.Conditions.contains( ClientID) ) return ;
+            this.Conditions.add( ClientID);
+        }
+    public void PastSessions(String ClientID ) 
+        {
+            Set<String> PastSessions =new HashSet<>(Summary);
+            if( PastSessions.contains( ClientID) ) return ;
+            this.PastSessions.add(ClientID);
+
+        }
+
+
 }
 
